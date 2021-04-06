d9972094f465d28794dda941ec2c542af02473f9
moritz löser <moritz.loeser@prodyna.com>
2021-04-06 15:41:34 +0200

    add cors to develop server, add dockerfile using apache httpd

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+var cors = require('cors');
 const app = express();
 const port = 3000;
+app.use(cors());
 app.use(express.static("src"));
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
